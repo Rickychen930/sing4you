c061bc058b3331e119506b3cd79900a0c4440d87
blackver69 <46539728+blackver69@users.noreply.github.com>
2026-01-25 21:13:17 +1100

    fix: auth controlller

--- src/server/controllers/AuthController.ts
+++ src/server/controllers/AuthController.ts
@@ -1,4 +1,4 @@
-import type { Request, Response, NextFunction } from 'express';
+import type { Request, Response } from 'express';
 import { AuthService } from '../services/AuthService';
 
 export class AuthController {
